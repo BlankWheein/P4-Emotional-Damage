00c44578b752766c29fe7a726bf04f3a27756b82
sebxGit <72699667+sebxGit@users.noreply.github.com>
2022-03-21 09:05:08 +0100

    first iteration of the lexer

---
+++ Compiler/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace P4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) return;
+
+
+                var lexer = new Lexer(line);
+                while (true)
+                {
+                    var token = lexer.NextToken();
+                    if (token.Kind == SyntaxKind.EndOfFileToken) break;
+
+                    Console.WriteLine($"{token.Kind}: '{token.Text}'");
+                    if (token.Value != null)
+                        Console.WriteLine($" {token.Value}");
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
+        {
+            var marker = isLast ? "└──" : "├──";
+
+            Console.Write(indent);
+            Console.Write(marker);
+            Console.Write(node.Kind);
+
+            if (node is SyntaxToken t && t.Value != null)
+            {
+                Console.Write(" ");
+                Console.Write(t.Value);
+            }
+
+            Console.WriteLine();
+
+            indent += isLast ? "    " : "│   ";
+
+            var lastChild = node.GetChildren().LastOrDefault();
+
+            foreach (var child in node.GetChildren())
+                PrettyPrint(child, indent, child == lastChild);
+        }
+    }
+}
---
+++ Compiler/SyntaxNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace P4
+{
+    abstract class SyntaxNode
+    {
+        public abstract SyntaxKind Kind { get; }
+
+        public abstract IEnumerable<SyntaxNode> GetChildren();
+    }
+
+}
