e5cd19bdf92de212f832a19fe768d08fec3ab892
Blank <47646799+BlankWheein@users.noreply.github.com>
2022-03-28 12:57:14 +0200

    Update RunTests.bat

--- RealGrammer/RunTests.bat
+++ RealGrammer/RunTests.bat
@@ -1,4 +1,174 @@
+cls
 @echo off
-echo -------------------------------------FunctionParameterTest-----------------------------------------------
+echo -------------------------------------FunctionParameterTest----------------------------------------------- : 2
 call grun MLgramma prog -diagnostics Tests/FunctionParameterTest.java
-echo Expected Errors 2  ---------------------------------------------------------------------------------------
+
+echo:
+echo:
+
+echo -------------------------------------         Minus             -----------------------------------------------
+echo -------------------------------------         DIncDouble       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/DIncDouble.java
+
+
+echo:
+echo:
+
+echo -------------------------------------         DIncFloat       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/DIncFloat.java
+
+echo:
+echo:
+
+echo -------------------------------------         DIncInt       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/DIncInt.java
+
+echo:
+echo:
+
+echo -------------------------------------         DIncString       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/DIncString.java
+
+echo:
+echo:
+
+echo -------------------------------------         MinusfloatDouble1       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/MinusfloatDouble1.java
+
+echo:
+echo:
+
+echo -------------------------------------         MinusfloatDouble2       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/MinusfloatDouble2.java
+
+
+echo:
+echo:
+
+echo -------------------------------------         Minusintdouble1       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/Minusintdouble1.java
+
+echo:
+echo:
+
+echo -------------------------------------         Minusintdouble2       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/Minusintdouble2.java
+
+echo:
+echo:
+
+echo -------------------------------------         Minusintfloat1       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/Minusintfloat1.java
+
+echo:
+echo:
+
+echo -------------------------------------         Minusintfloat2       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/Minusintfloat2.java
+
+echo:
+echo:
+
+echo -------------------------------------         Minusintint       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/Minusintint.java
+
+echo:
+echo:
+
+echo -------------------------------------         Minusintstring1       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/Minusintstring1.java
+
+echo:
+echo:
+
+echo -------------------------------------         Minusintstring2       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/minus/Minusintstring2.java
+
+
+echo:
+echo:
+
+echo -------------------------------------         IncDouble       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/IncDouble.java
+
+echo:
+echo:
+
+echo -------------------------------------         IncFloat       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/IncFloat.java
+
+echo:
+echo:
+
+echo -------------------------------------         IncInt       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/IncInt.java
+
+
+echo:
+echo:
+
+echo -------------------------------------         IncString       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/IncString.java
+
+echo:
+echo:
+
+echo -------------------------------------         plusintdouble1       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/plusintdouble1.java
+
+
+echo:
+echo:
+
+echo -------------------------------------         plusintdouble2       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/plusintdouble2.java
+
+echo:
+echo:
+
+echo -------------------------------------         plusintfloat1       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/plusintfloat1.java
+
+echo:
+echo:
+
+echo -------------------------------------         plusintfloat2       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/plusintfloat2.java
+
+echo:
+echo:
+
+echo -------------------------------------         plusintint       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/plusintint.java
+
+echo:
+echo:
+
+echo -------------------------------------         plusintstring1       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/plusintstring1.java
+
+
+echo:
+echo:
+
+echo -------------------------------------         plusintstring2       ----------------------------------------------- : 0
+call grun MLgramma prog -diagnostics Tests/plus/plusintstring2.java
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
