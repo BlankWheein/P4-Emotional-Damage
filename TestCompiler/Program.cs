06c8f6470d246a33060b2404e370bf9a478a693c
Blank <47646799+BlankWheein@users.noreply.github.com>
2022-03-31 21:50:39 +0200

    Update Program.cs

--- TestCompiler/Program.cs
+++ TestCompiler/Program.cs
@@ -19,7 +19,6 @@ namespace TestCompiler
                 StringBuilder text = new StringBuilder();
                 Console.WriteLine("> Input source code: ");
 
-                // to type the EOF character and end the input: use CTRL+D, then press <enter>
                 while ((input = Console.ReadLine()) != "")
                 {
                     text.AppendLine(input);
@@ -27,9 +26,6 @@ namespace TestCompiler
                 if (text.Length == 0)
                 {
                     string testLine = "int kage = 2;if (1==1){int kage2 = 2;int akwd = 2; print kage;}if (1==1){int dkawp = 2;int kdwa = 3; print kdwa; if (1==1){if (2==2){int kage3 = 3;print kage3;}}} print \"done\"";
-                    //string testLine = "int kage = 2;if (1){int kage = 2;int akwd = 2;} elif(1) {kage = 2; int test = 4;}elif(1) {kage = 6; int test2 = 4;} else {int kage = 2;}";
-                    //string testLine = "int kage = 2;if (1){int kage = 2;int akwd = 2;} elif(1) {kage = 2; int test = 4;}elif(1) {kage = 6; int test2 = 4;}";
-                    //string testLine = "int kage = 3;if (1){kage = 4; int test = 3;}";
                     Console.WriteLine(testLine);
                     Console.WriteLine();
                     text.AppendLine(testLine);
@@ -37,8 +33,6 @@ namespace TestCompiler
                 Compiler compiler = new(text);
                 compiler.Compile();
 
-                
-            
             }
             catch (Exception ex)
             {
