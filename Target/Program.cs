f2ec91d4fd1c30659481f6a31d60cb20e8d73a91
Blank <47646799+BlankWheein@users.noreply.github.com>
2022-03-31 23:55:15 +0200

    Updated to .Net 6

--- Target/Program.cs
+++ Target/Program.cs
@@ -1,16 +1,23 @@
 Console.ForegroundColor = ConsoleColor.DarkYellow;
-int kage = 0;
-for (int i = kage; i<10; i++) {
-    if (i==1) {
-        Console.WriteLine(10%2);
+int kage = 2;
+if (1==1) {
+    int kage2 = 2;
+    int akwd = 2;
+    Console.WriteLine(kage);
+    Test();
+}
+if (1==1) {
+    int dkawp = 2;
+    int kdwa = 3;
+    Console.WriteLine(kdwa);
+    if (1==1) {
+        if (2==2) {
+            int kage3 = 3;
+            Console.WriteLine(kage3);
+        }
     }
-    else if (i==2) {
-        Console.WriteLine(i+2);
-    }
-    else if (i==7) {
-        Console.WriteLine(i-69);
-    }
-    else {
-        Console.WriteLine("Hej");
-    }
+}
+void Test()
+{
+    Console.WriteLine("HelloThere");
 }
--- TestCompiler/Program.cs
+++ TestCompiler/Program.cs
@@ -4,44 +4,24 @@ using System.Collections.Generic;
 using System.Text;
 using TestCompiler.Steps;
 using static TestGrammarParser;
-namespace TestCompiler
-{
+    string? input = "";
+    StringBuilder text = new StringBuilder();
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("> Input source code: ");
 
-    internal static partial class Program
-{
-    public static void Main()
+    while ((input = Console.ReadLine()) != "")
     {
-        while (true)
-        {
-            try
-            {
-                string? input = "";
-                StringBuilder text = new StringBuilder();
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("> Input source code: ");
-
-                while ((input = Console.ReadLine()) != "")
-                {
-                    text.AppendLine(input);
-                }
-                if (text.Length == 0)
-                {
-                    //string testLine = "int kage = 2;if (1==1){int kage2 = 2;int akwd = 2; print kage;}if (1==1){int dkawp = 2;int kdwa = 3; print kdwa; if (1==1){if (2==2){int kage3 = 3;print kage3;}}} print \"Hello There\";";
-                    string testLine = "int kage = 0; for (int i = kage; i < 10; i++) { if (i == 1) { print 10%2; } elif(i == 2) { print i + 2; } elif(i == 7) { print i - 69; } else { print \"Hej\"; }}";
-                    Console.WriteLine(testLine);
-                    Console.WriteLine();
-                    text.AppendLine(testLine);
-                }
-                Compiler compiler = new(text);
-                compiler.Compile();
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex);
-            }
-            
-        }
+        text.AppendLine(input);
     }
-}
-}
+    if (text.Length == 0)
+    {
+        string testLine = "int kage = 2;if (1==1){int kage2 = 2;int akwd = 2; print kage;}if (1==1){int dkawp = 2;int kdwa = 3; print kdwa; if (1==1){if (2==2){int kage3 = 3;print kage3;}}} print \"Hello There\";";
+        //string testLine = "int kage = 0; for (int i = kage; i < 10; i++) { if (i == 1) { print 10%2; } elif(i == 2) { print i + 2; } elif(i == 7) { print i - 69; } else { print \"Hej\"; }}";
+        Console.WriteLine(testLine);
+        Console.WriteLine();
+        text.AppendLine(testLine);
+    }
+    Compiler compiler = new(text);
+    compiler.Compile();
+
+            
