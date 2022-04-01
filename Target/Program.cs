38606cb7ec2e72b0493b369f4b296ec88b461ecf
SebxGit <minhseb@gmail.com>
2022-04-01 11:46:40 +0200

    Fixed float and double code generation and automatically adds 0 if e.g. 3. is typed

--- Target/Program.cs
+++ Target/Program.cs
@@ -1,5 +1,6 @@
 Console.ForegroundColor = ConsoleColor.DarkYellow;
 int kage = 2;
-int test = 3;
-Console.WriteLine(kage);
-Console.WriteLine(test);
+float abe = 3.2f;
+double abekat = 3.2d;
+float irri = 3.0f;
+double irr = 2.0d;
