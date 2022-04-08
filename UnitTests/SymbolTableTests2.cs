22b917fbe8882e44ed3b07140d23c3694f0e2bf9
Obeline <62429595+Obeline@users.noreply.github.com>
2022-04-08 14:31:36 +0200

    Some unit tests

--- UnitTests/SymbolTableTests2.cs
+++ UnitTests/SymbolTableTests2.cs
@@ -6,14 +6,42 @@ namespace UnitTests
     {
 
         [TestMethod]
-        public void TestMehod2()
+        public void Insert_Symbol_ID_Is_Reserved_Keyword_Expected_Diagnostics_Count_Is_1()
         {
-            var scope = new RootSymbolTable();
-            scope.Allocate();
-            scope.Current.Insert(0, "kage", false);
-            scope.ExitScope();
-            var kage = scope.LookUp("kage");
-            Assert.AreEqual(null, kage);
+            var Root = new RootSymbolTable();
+            Symbol symbol = new("hej");
+            Root.Insert(symbol);
+
+            Assert.IsTrue(Root.Diagnostics.Count == 1);
+            Assert.IsTrue(Root.Symbols.Count == 0);
+        }
+        [TestMethod]
+        public void Insert_Symbol_ID_Is_In_SymbolTable_Expected_Diagnostics_Count_Is_1()
+        {
+            var Root = new RootSymbolTable();
+            Symbol symbol = new("a");
+            Symbol symbol2 = new("a");
+            Root.Insert(symbol);
+            Root.Insert(symbol2);
+
+            Assert.IsTrue(Root.Symbols.Count == 1);
+            Assert.IsTrue(Root.Diagnostics.Count == 1);
+        }
+
+
+        [TestMethod]
+        public void Allocate_3_SymbolTables_Expected_Equal_Count()
+        {
+            var Root = new RootSymbolTable();
+            Symbol symbol = new("a");
+            Root.Allocate();
+
+            SymbolTable table1 = Root.Current;
+            Root.Allocate();
+            SymbolTable table2 = Root.Current;
+            table2.Insert(symbol);
+
+            Assert.AreEqual(table2.Symbols.Count, Root.Children[0].Children[0].Symbols.Count);
         }
     }
 }
