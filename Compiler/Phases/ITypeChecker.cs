dad96a8b71c6a95d313ef08801672d731d6f8f0c
Obeline <62429595+Obeline@users.noreply.github.com>
2022-04-12 15:16:09 +0200

    for numbers

---
+++ Compiler/Phases/ITypeChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EmotionalDamageParser;
+namespace Compiler.Phases
+{
+    interface ITypeChecker
+    {
+        public void EvaluateMatrixExpr(MatrixarrexprContext context, string type);
+        public void EvaluateMatrixDimensions();
+        public void EvaluateNumExpr(NumexprContext context, string type);
+
+    }
+}
---
+++ Compiler/Phases/TypeChecker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Compiler.SymbolTableFolder;
+using static EmotionalDamageParser;
+
+namespace Compiler.Phases
+{
+    class TypeChecker
+    {
+        //typecasting
+        //typechecking, correct type, and compatible operations
+        public ScopeVisitor Parent { get; }
+
+        public TypeChecker(ScopeVisitor parent)
+        {
+            Parent = parent;
+        }
+
+
+        public bool EvaluateNumExpr(NumexprContext context, SymbolType type)
+        {
+            bool isDef = true;
+            if (context.val() != null)
+            {
+                if (context.val().num() != null)
+                {
+                    isDef &= type switch //check if number is int or float. If not, isDef will be false
+                    {
+                        SymbolType.Int => int.TryParse(context.val().GetText(), out int _),
+                        SymbolType.Float=> float.TryParse(context.val().GetText(), out float _),
+                        _ => false,
+                    };
+
+                }
+                else
+                {
+                    isDef &= type == Parent?.Scope?.LookUp(context?.val()?.id()?.GetText()?.Trim('"'))?.Type;
+                }
+            }
+            else
+            {
+                foreach (NumexprContext e in context.numexpr())
+                    isDef &= EvaluateNumExpr(e, type);
+            }
+            return isDef;
+        }
+    }
+}
