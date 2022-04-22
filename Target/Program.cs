988a51f8283d06c2e01641c72622c0643aae64fe
SebxGit <minhseb@gmail.com>
2022-04-22 14:12:59 +0200

    all declarations works now !

--- Target/Program.cs
+++ Target/Program.cs
@@ -7,5 +7,5 @@ int[] ad = new int[5];
 float[] ad2 = new float[3];
 string[] ad3 = new string[4];
 int af = 2+2;
-string str = "1241qwesjfidfnhsdi"
-bool hallo = true
+string str = "1241qwesjfidfnhsdi";
+bool hallo = true;
