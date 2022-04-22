9559aeffa74daba5e302d4aba0c87512b7a81608
Casper Andersen <casper11andersen@gmail.com>
2022-04-22 14:50:43 +0200

    idk how to do the matrix stuff

--- Compiler/Classes/Matrix.cs
+++ Compiler/Classes/Matrix.cs
@@ -1,13 +1,18 @@
+using System.Collections.Generic;
 namespace Compiler.Classes
 {
     public class Matrix{
         public string Id { get; set; }
         public int Row {get; set; }
         public int Column {get; set; }
-        public Matrix(int row, int column, string id){
+        
+        public Matrix(string type,string id, int row, int column ){
             Row = row; 
             Column = column; 
             Id = id;
+            
         }
+
+        
     }
 }
