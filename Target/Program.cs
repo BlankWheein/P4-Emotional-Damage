116b4c9aba2af28c93b576ef56eaf3ef2da803c3
SebxGit <minhseb@gmail.com>
2022-04-25 15:46:34 +0200

    updated and deleted Avas (didnt work)

--- Target/Program.cs
+++ Target/Program.cs
@@ -1,6 +1,6 @@
 using AutoGrad;
 
-float x = MathF.Sqrt(0 + 20);
+float x = MathF.Sqrt(10 + 20);
 float a = MathF.Pow(2, 10);
 float b = 35 % 10;
 float c = 23 * 52;
@@ -19,7 +19,7 @@ Console.WriteLine("hello");
 int h = mar.Rows;
 int i = mar.Columns;
 int j = mon.Length;
-float k = MathF.Sqrt();
+float k = MathF.Sqrt(b);
 float kage (float a, float b) {
 }
 kage();
