99f13182973433ddaa0eac95cc2c48c10ca03b27
Casper Andersen <54134828+GrimmFantasy@users.noreply.github.com>
2022-04-26 11:28:02 +0200

    Update TranslateToCsTests.cs

--- UnitTests/TranslateToCsTests.cs
+++ UnitTests/TranslateToCsTests.cs
@@ -26,9 +26,24 @@ namespace ScopeTests
         }
         [TestMethod]
         public void PowTest1() {
-            string res = "MathF.Pow(x,132);";
-            string input = _codeGen.CheckExpr("x**132;");
+            string res = "MathF.Pow(x, 132)";
+            string input = _codeGen.CheckExpr("x**132");
             Assert.AreEqual(input, res);
+        }
+
+        [TestMethod]
+        public void PowTest2() {
+            string res = "x**132";
+            string input = _codeGen.CheckExpr("x**132");
+            Assert.AreNotEqual(input, res);
+        }
+
+        [TestMethod]
+        public void NestedExprTest()
+        {
+            string res = "x + 34 *(MathF.sqrt(MathF.Pow(x, 4)))";
+            string input = _codeGen.CheckExpr("x + 34 *(sqrt(x**4))");
+            Assert.AreEqual(res, input);
         }
     }
 }
