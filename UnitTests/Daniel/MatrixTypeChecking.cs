5329a611d55eb6ae61305487a69285168cf38c43
Obeline <62429595+Obeline@users.noreply.github.com>
2022-04-27 13:26:59 +0200

    added some matrix unit testz

--- UnitTests/Daniel/MatrixTypeChecking.cs
+++ UnitTests/Daniel/MatrixTypeChecking.cs
@@ -10,14 +10,14 @@ using Compiler.SymbolTableFolder;
 namespace UnitTests.Daniel
 {
     [TestClass]
-    class MatrixTypeChecking : UnitTestInitializer
+    public class MatrixTypeChecking : UnitTestInitializer
     {
 
         [TestMethod]
         public void MatrixDCLSuccess()
         {
-            var root = Parse(new StringBuilder("float kage = sqrt 5 ;"));
-            scope.Insert(SymbolType.Float, "kage");
+            var root = Parse(new StringBuilder("int[2][4] kage;"));
+            scope.Insert(SymbolType.Mint, "kage",2,4);
             Assert.AreEqual(scope, root);
             Assert.AreEqual(0, root.Diagnostics.Count);
         }
@@ -25,28 +25,49 @@ namespace UnitTests.Daniel
         [TestMethod]
         public void MatrixDCLFail()
         {
+            var root = Parse(new StringBuilder("int[0][2] kage;"));
+            scope.Insert(SymbolType.Mint, "kage", 0, 2);
+            Assert.AreNotEqual(scope, root);
+            Assert.AreEqual(1, root.Diagnostics.Count);
 
         }
 
         [TestMethod]
         public void MatrixAssignSuccess()
         {
+            var root = Parse(new StringBuilder("int[2][2] kage; kage[1][0]=5;kage[1][0]=7; kage[0][0]=5;"));
+            Assert.AreEqual(0, root.Diagnostics.Count);
         }
 
 
         [TestMethod]
         public void MatrixAssignIndexOutOfBounds()
         {
+            var root = Parse(new StringBuilder("int[2][2] kage; kage[2][2]=5;kage[4][2]=7;"));
+            Assert.AreEqual(4, root.Diagnostics.Count);
+
         }
 
         [TestMethod]
         public void MatrixAssignIDNotDeclared()
         {
+            var root = Parse(new StringBuilder("int[2][2] kage; kage[a][1]=5;"));
+            Assert.AreEqual(1, root.Diagnostics.Count);
         }
 
         [TestMethod]
         public void MatrixAssignIDNotInt()
         {
+            var root = Parse(new StringBuilder("int[2][2] kage;float a=3.1; kage[a][1]=5;"));
+            Assert.AreEqual(1, root.Diagnostics.Count);
+        }
+
+
+        [TestMethod]
+        public void MatrixAssignMultiErrors()
+        {
+            var root = Parse(new StringBuilder("int[2][2] kage; int[2][2] kage2; float a=3.1; kage[a][8]=5; kage2[10,b]=5;"));
+            Assert.AreEqual(4, root.Diagnostics.Count);
         }
     }
 }
