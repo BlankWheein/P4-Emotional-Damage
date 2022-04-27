e90b7dbdd7c582bbf03dafdcde9c059105582670
D-Kuma-C <dberwa20@student.aau.dk>
2022-04-27 12:39:47 +0200

    Update FuncChecking.cs

--- UnitTests/Daniel/FuncChecking.cs
+++ UnitTests/Daniel/FuncChecking.cs
@@ -119,13 +119,83 @@ namespace UnitTests.Daniel
         public void FuncVoidReturn()
         {
             var root = Parse(new StringBuilder("void kage() {int z = 6; float x = 6.9; return x;}"));
-            scope.Insert(SymbolType.Float, "kage", parameters: new List<Symbol>() { }, isfunc: true);
+            scope.Insert(SymbolType.Void, "kage", parameters: new List<Symbol>() { }, isfunc: true);
             scope.Allocate("Func");
             scope.Insert(SymbolType.Int, "z");
             scope.Insert(SymbolType.Float, "x");
             scope.ExitScope();
             Assert.AreEqual(scope, root);
             Assert.AreEqual(1, root.Diagnostics.Count);
+        }
+
+        [TestMethod]
+        public void FuncCall()
+        {
+            var root = Parse(new StringBuilder("float kage() {int z = 6; float x = 6.9; return x;} kage();"));
+            scope.Insert(SymbolType.Float, "kage", parameters: new List<Symbol>() { }, isfunc: true);
+            scope.Allocate("Func");
+            scope.Insert(SymbolType.Int, "z");
+            scope.Insert(SymbolType.Float, "x");
+            scope.ExitScope();
+            Assert.AreEqual(scope, root);
+            Assert.AreEqual(0, root.Diagnostics.Count);
+        }
+         [TestMethod]
+        public void FuncCallWrongParameter()
+        {
+            var root = Parse(new StringBuilder("float kage() {int z = 6; float x = 6.9; return x;} int kage2; kage(kage2);"));
+            scope.Insert(SymbolType.Float, "kage", parameters: new List<Symbol>() { }, isfunc: true);
+            scope.Allocate("Func");
+            scope.Insert(SymbolType.Int, "z");
+            scope.Insert(SymbolType.Float, "x");
+            scope.ExitScope();
+            scope.Insert(SymbolType.Int, "kage2");
+            Assert.AreEqual(scope, root);
+            Assert.AreEqual(1, root.Diagnostics.Count);
+        }
+
+        public void FuncCallRightParameter()
+        {
+            var root = Parse(new StringBuilder("float kage(int x) {float z = 6.9 + x; return z;} int kage2 = 5; kage(kage2);"));
+            scope.Insert(SymbolType.Float, "kage", parameters: new List<Symbol>() { new("x", SymbolType.Int)}, isfunc: true);
+            scope.Allocate("Func");
+            scope.Insert(SymbolType.Float, "z");
+            scope.ExitScope();
+            scope.Insert(SymbolType.Int, "kage2");
+            Assert.AreEqual(scope, root);
+            Assert.AreEqual(0, root.Diagnostics.Count);
+        }
+
+        [TestMethod]
+        public void FuncCallWrongParameters()
+        {
+            var root = Parse(new StringBuilder("float kage(int x, float y, string elite) {int z = 6; float x = 6.9; return x;} float kage2 = 6.9; int kage3 = 75; bool kage4 = true; int kage(kage2, kage3, kage4);"));
+            scope.Insert(SymbolType.Float, "kage", parameters: new List<Symbol>() { new("x", SymbolType.Int), new("y", SymbolType.Float), new("elite", SymbolType.String)}, isfunc: true);
+            scope.Allocate("Func");
+            scope.Insert(SymbolType.Int, "z");
+            scope.Insert(SymbolType.Float, "x");
+            scope.ExitScope();
+            scope.Insert(SymbolType.Float, "kage2");
+            scope.Insert(SymbolType.Int, "kage3");
+            scope.Insert(SymbolType.Bool, "kage4");
+            Assert.AreEqual(scope, root);
+            Assert.AreEqual(3, root.Diagnostics.Count);
+        }
+
+        [TestMethod]
+        public void FuncCallRightParameters()
+        {
+            var root = Parse(new StringBuilder("float kage(int x, float y, string elite) {int z = 6; float x = 6.9; return x;} int kage2 = 6.9; float kage3 = 75; string kage4 = \"Din mor\"; int kage(kage2, kage3, kage4);"));
+            scope.Insert(SymbolType.Float, "kage", parameters: new List<Symbol>() { new("x", SymbolType.Int), new("y", SymbolType.Float), new("elite", SymbolType.String) }, isfunc: true);
+            scope.Allocate("Func");
+            scope.Insert(SymbolType.Int, "z");
+            scope.Insert(SymbolType.Float, "x");
+            scope.ExitScope();
+            scope.Insert(SymbolType.Int, "kage2");
+            scope.Insert(SymbolType.Float, "kage3");
+            scope.Insert(SymbolType.String, "kage4");
+            Assert.AreEqual(scope, root);
+            Assert.AreEqual(0, root.Diagnostics.Count);
         }
     }
 }
