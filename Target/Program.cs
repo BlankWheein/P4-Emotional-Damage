1d75e9e1f678936948d97e952f1fce5fcb2b5c8b
SebxGit <minhseb@gmail.com>
2022-04-27 13:41:34 +0200

    fixed edge case with functions inside expr

---
+++ Target/Program.cs
@@ -0,0 +1,14 @@
+using AutoGrad;
+
+int kage (int a) {
+    return a;
+}
+float b = (kage(2 + 3) + 32);
+float md = (32 + 235 + 62);
+float a = 2 + MathF.Pow(12 + 10 + Kage(2 + 3),12 + 125) * 24 / 10;
+float m = 20 + MathF.Sqrt(10 + 23 * MathF.Sqrt(10)) + 2;
+float a = 23 * 123 % 235 + 235221 - 124513 + 200 - 123214;
+float c = 12 + MathF.Pow(4,20) + 3;
+float c = MathF.Pow(12 + 4,20 + 3);
+float ma = 2 + 1 \\\\ b;
+Console.WriteLine("2+3");
