17384ec00941007447861731549997f4e9900bfd
D-Kuma-C <dberwa20@student.aau.dk>
2022-04-28 13:10:37 +0200

    Update FuncChecking.cs

--- UnitTests/Daniel/FuncChecking.cs
+++ UnitTests/Daniel/FuncChecking.cs
@@ -287,13 +287,13 @@ namespace UnitTests.Daniel
         public void FuncArrSucces()
         {
             var root = Parse(new StringBuilder("int[3] kage(int[2] a, int[3] a2) {int[3] ice = a[1] + a2[2]; return ice;} int[2] arr; int[3] lol; final[3] = kage(arr, lol);"));
-            scope.Insert(SymbolType.Aint, "kage", 3, parameters: new List<Symbol>() { new("a", SymbolType.Aint, 2), new("a2", SymbolType.Aint, 3) }, isfunc: true);
+            scope.Insert(SymbolType.Aint, "kage", row: 3, parameters: new List<Symbol>() { new("a", SymbolType.Aint, row: 2), new("a2", SymbolType.Aint, row: 3) }, isfunc: true);
             scope.Allocate("Func");
-            scope.Insert(SymbolType.Aint, "ice", 3);
+            scope.Insert(SymbolType.Aint, "ice", row: 3);
             scope.ExitScope();
-            scope.Insert(SymbolType.Aint, "arr", 2);
-            scope.Insert(SymbolType.Aint, "lol", 3);
-            scope.Insert(SymbolType.Aint, "final", 3);
+            scope.Insert(SymbolType.Aint, "arr", row: 2);
+            scope.Insert(SymbolType.Aint, "lol", row: 3);
+            scope.Insert(SymbolType.Aint, "final", row: 3);
             Assert.AreEqual(scope, root);
             Assert.AreEqual(0, root.Diagnostics.Count);
         }
@@ -302,13 +302,13 @@ namespace UnitTests.Daniel
         public void FuncArrFails()
         {
             var root = Parse(new StringBuilder("int[3] kage(int[2] a, int[3] a2) {int[2] ice = a[1] + a2[2]; return ice;} int[2] arr; int[3] lol; final[1] = kage(arr, lol);"));
-            scope.Insert(SymbolType.Aint, "kage", 3, parameters: new List<Symbol>() { new("a", SymbolType.Aint, 2), new("a2", SymbolType.Aint, 3) }, isfunc: true);
+            scope.Insert(SymbolType.Aint, "kage", row: 3, parameters: new List<Symbol>() { new("a", SymbolType.Aint, row: 2), new("a2", SymbolType.Aint, row: 3) }, isfunc: true);
             scope.Allocate("Func");
-            scope.Insert(SymbolType.Aint, "ice", 2);
+            scope.Insert(SymbolType.Aint, "ice", row: 2);
             scope.ExitScope();
-            scope.Insert(SymbolType.Aint, "arr", 2);
-            scope.Insert(SymbolType.Aint, "lol", 3);
-            scope.Insert(SymbolType.Aint, "final", 1);
+            scope.Insert(SymbolType.Aint, "arr", row: 2);
+            scope.Insert(SymbolType.Aint, "lol", row: 3);
+            scope.Insert(SymbolType.Aint, "final", row: 1);
             Assert.AreEqual(scope, root);
             Assert.AreEqual(2, root.Diagnostics.Count);
         }
@@ -317,12 +317,12 @@ namespace UnitTests.Daniel
         public void FuncWrongArrDim()
         {
             var root = Parse(new StringBuilder("int[2] kage(int[2] a) {int[2] ice = a[1]; return ice;} int[1] arr; final[2] = kage(arr);"));
-            scope.Insert(SymbolType.Aint, "kage", 2, parameters: new List<Symbol>() { new("a", SymbolType.Aint, 2)}, isfunc: true);
+            scope.Insert(SymbolType.Aint, "kage", row: 2, parameters: new List<Symbol>() { new("a", SymbolType.Aint, row: 2)}, isfunc: true);
             scope.Allocate("Func");
-            scope.Insert(SymbolType.Aint, "ice", 2);
+            scope.Insert(SymbolType.Aint, "ice", row: 2);
             scope.ExitScope();
-            scope.Insert(SymbolType.Aint, "arr", 1);
-            scope.Insert(SymbolType.Aint, "final", 2);
+            scope.Insert(SymbolType.Aint, "arr", row: 1);
+            scope.Insert(SymbolType.Aint, "final", row: 2);
             Assert.AreEqual(scope, root);
             Assert.AreEqual(1, root.Diagnostics.Count);
         }
@@ -331,12 +331,12 @@ namespace UnitTests.Daniel
         public void FuncWrongArrDim2()
         {
             var root = Parse(new StringBuilder("int[2] kage(int[2] a) {int[2] ice = a[1]; return ice;} int[5] arr; final[2] = kage(arr);"));
-            scope.Insert(SymbolType.Aint, "kage", 2, parameters: new List<Symbol>() { new("a", SymbolType.Aint, 2) }, isfunc: true);
+            scope.Insert(SymbolType.Aint, "kage", row: 2, parameters: new List<Symbol>() { new("a", SymbolType.Aint, row: 2) }, isfunc: true);
             scope.Allocate("Func");
-            scope.Insert(SymbolType.Aint, "ice", 2);
+            scope.Insert(SymbolType.Aint, "ice", row: 2);
             scope.ExitScope();
-            scope.Insert(SymbolType.Aint, "arr", 5);
-            scope.Insert(SymbolType.Aint, "final", 2);
+            scope.Insert(SymbolType.Aint, "arr", row: 5);
+            scope.Insert(SymbolType.Aint, "final", row: 2);
             Assert.AreEqual(scope, root);
             Assert.AreEqual(1, root.Diagnostics.Count);
         }
@@ -344,14 +344,14 @@ namespace UnitTests.Daniel
         [TestMethod]
         public void FuncMatSucces()
         {
-            var root = Parse(new StringBuilder("int[3][3] kage(int[2][2] m, int[3][3] m2) {int[3][3] ice = m[1][1] + m2[2][2]; return ice;} int[2][2] mat; int[3][3] lol; final[3][3] = kage(arr, lol);"));
-            scope.Insert(SymbolType.Mint, "kage", 3, 3, parameters: new List<Symbol>() { new("m", SymbolType.Aint, 2, 2), new("m2", SymbolType.Aint, 3, 3) }, isfunc: true);
+            var root = Parse(new StringBuilder("int[3][3] kage(int[2][2] m, int[3][3] m2) {int[3][3] ice = m[1][1] + m2[2][2]; return ice;} int[2][2] mat; int[3][3] lol; final[3][3] = kage(mat, lol);"));
+            scope.Insert(SymbolType.Mint, "kage", row: 3, col: 3, parameters: new List<Symbol>() { new("m", SymbolType.Aint, row: 2, col: 2), new("m2", SymbolType.Aint, row: 3, col: 3) }, isfunc: true);
             scope.Allocate("Func");
-            scope.Insert(SymbolType.Mint, "ice", 3, 3);
+            scope.Insert(SymbolType.Mint, "ice", row: 3, col: 3);
             scope.ExitScope();
-            scope.Insert(SymbolType.Mint, "mat", 2, 2);
-            scope.Insert(SymbolType.Mint, "lol", 3, 3);
-            scope.Insert(SymbolType.Mint, "final", 3, 3);
+            scope.Insert(SymbolType.Mint, "mat", row: 2, col: 2);
+            scope.Insert(SymbolType.Mint, "lol", row: 3, col: 3);
+            scope.Insert(SymbolType.Mint, "final", row: 3, col: 3);
             Assert.AreEqual(scope, root);
             Assert.AreEqual(0, root.Diagnostics.Count);
         }
@@ -359,14 +359,44 @@ namespace UnitTests.Daniel
         [TestMethod]
         public void FuncMatFails()
         {
-            var root = Parse(new StringBuilder("int[3][1] kage(int[1][2] m, int[9][3] m2) {int[3][3] ice = m[1][1] + m2[2][2]; return ice;} int[2][2] mat; int[3][3] lol; final[3][5] = kage(arr, lol);"));
-            scope.Insert(SymbolType.Mint, "kage", 3, 3, parameters: new List<Symbol>() { new("m", SymbolType.Aint, 1, 2), new("m2", SymbolType.Aint, 9, 3) }, isfunc: true);
+            var root = Parse(new StringBuilder("int[3][1] kage(int[1][2] m, int[9][3] m2) {int[3][3] ice = m[1][1] + m2[2][2]; return ice;} int[2][2] mat; int[3][3] lol; final[3][5] = kage(mat, lol);"));
+            scope.Insert(SymbolType.Mint, "kage", row: 3, col: 3, parameters: new List<Symbol>() { new("m", SymbolType.Aint, row: 1, col: 2), new("m2", SymbolType.Aint, row: 9, col: 3) }, isfunc: true);
             scope.Allocate("Func");
-            scope.Insert(SymbolType.Mint, "ice", 3, 3);
+            scope.Insert(SymbolType.Mint, "ice", row: 3, col: 3);
             scope.ExitScope();
-            scope.Insert(SymbolType.Mint, "mat", 2, 2);
-            scope.Insert(SymbolType.Mint, "lol", 3, 3);
-            scope.Insert(SymbolType.Mint, "final", 3, 5);
+            scope.Insert(SymbolType.Mint, "mat", row: 2, col: 2);
+            scope.Insert(SymbolType.Mint, "lol", row: 3, col: 3);
+            scope.Insert(SymbolType.Mint, "final", row: 3, col: 5);
+            Assert.AreEqual(scope, root);
+            Assert.AreEqual(3, root.Diagnostics.Count);
+        }
+
+        [TestMethod]
+        public void FuncMatDim()
+        {
+            var root = Parse(new StringBuilder("int[3][3] kage(int[3][3] m) {int[3][3] ice = m[1][1]; return ice;} int[2][2] mat; final[3][3] = kage(mat);"));
+            scope.Insert(SymbolType.Mint, "kage", row: 3, col: 3, parameters: new List<Symbol>() { new("m", SymbolType.Aint, row: 1, col: 2)}, isfunc: true);
+            scope.Allocate("Func");
+            scope.Insert(SymbolType.Mint, "ice", row: 3, col: 3);
+            scope.ExitScope();
+            scope.Insert(SymbolType.Mint, "mat", row: 2, col: 2);
+            scope.Insert(SymbolType.Mint, "lol", row: 3, col: 3);
+            scope.Insert(SymbolType.Mint, "final", row: 3, col: 5);
+            Assert.AreEqual(scope, root);
+            Assert.AreEqual(3, root.Diagnostics.Count);
+        }
+
+        [TestMethod]
+        public void FuncMatDim2()
+        {
+            var root = Parse(new StringBuilder("int[3][1] kage(int[1][2] m, int[9][3] m2) {int[3][3] ice = m[1][1] + m2[2][2]; return ice;} int[2][2] mat; int[3][3] lol; final[3][5] = kage(arr, lol);"));
+            scope.Insert(SymbolType.Mint, "kage", row: 3, col: 3, parameters: new List<Symbol>() { new("m", SymbolType.Aint, row: 1, col: 2), new("m2", SymbolType.Aint, row: 9, col: 3) }, isfunc: true);
+            scope.Allocate("Func");
+            scope.Insert(SymbolType.Mint, "ice", row: 3, col: 3);
+            scope.ExitScope();
+            scope.Insert(SymbolType.Mint, "mat", row: 2, col: 2);
+            scope.Insert(SymbolType.Mint, "lol", row: 3, col: 3);
+            scope.Insert(SymbolType.Mint, "final", row: 3, col: 5);
             Assert.AreEqual(scope, root);
             Assert.AreEqual(3, root.Diagnostics.Count);
         }
