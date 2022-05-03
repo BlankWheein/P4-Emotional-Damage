461a53f75d5eb772d1cd4834a3b01991c6f8e289
D-Kuma-C <dberwa20@student.aau.dk>
2022-05-03 08:35:58 +0200

    xccgb

--- Target/Program.cs
+++ Target/Program.cs
@@ -7,18 +7,4 @@ happy.Values[0][1] = new Value(1, CalculateGradient: false);
 int[] hap = new int[2];
 hap[0] = 1;
 hap[1] = 2;
-
-
-public static class IntExftensions
-{
-    public static string ToString(this int[] kage)
-    {
-        return ";";
-    }
-    public static string ToString(this float[] kage)
-    {
-        return ";";
-    }
-} 
-Console.WriteLine(happy);
-Console.WriteLine(hap.ToString());
+Console.Write($"{happy}");
