6f64744c84669093a359617fe96132559365c9e0
SebxGit <minhseb@gmail.com>
2022-05-03 09:59:38 +0200

    cleaned pow code

--- Target/Program.cs
+++ Target/Program.cs
@@ -1,39 +1,9 @@
 using AutoGrad;
 
-Value x = new Value(4, null,"x", true);
-float f = MathF.Sqrt(x.data + 4 * MathF.Sqrt(45));
-Value y = x.Pow(1 / 2);
-y.Backward();
-float h = x.grad;
-Matrix a = new(1,2);
-Matrix j = new(1,2);
-a.Values[0][0] = new Value(1);
-a.Values[0][1] = new Value(2);
-j.Values[0][0] = new Value(2);
-j.Values[0][1] = new Value(1);
-Matrix g = new(1,2);
-int s = 4;
-g = a - j;
-bool t = true;
-if(t==true){
-Console.WriteLine("hej");
+float kage (float a) {
+    return a;
 }
-if(x.data>=f){
-Console.WriteLine("hej");
-}
-if(x.data<f){
-Console.WriteLine("hej");
-}
-if(x.data<=f){
-Console.WriteLine("hej");
-}
-if(x.data==f){
-Console.WriteLine("hej");
-}
-if(x.data!=f){
-Console.WriteLine("hej");
-}
-float ttsqrt (float l) {
-    return l;
-}
-float xk = 4;
+float f = 2;
+float a = MathF.Pow(f + 2, 10);
+float b = MathF.Pow(230, 10);
+float c = MathF.Pow(20 + kage(f) + 30, 10 * 2456);
