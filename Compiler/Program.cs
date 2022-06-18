41ea61936c290a8120da345427e79b2bb8b1e694
Blank <47646799+BlankWheein@users.noreply.github.com>
2022-06-18 18:11:48 +0200

    Update Program.cs

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -8,17 +8,16 @@ string readText = File.ReadAllText("../../../Emotional.Damage");
 Console.WriteLine(readText);
 text.AppendLine(readText);
 Wrapper wrapper = new(text);
-wrapper.Compile();
-//if (wrapper.Compile())
-//{
-//    Process p = new();
-//    lock (Console.Out)
-//    {
-//        p = new Process();
-//        p.StartInfo.FileName = "cmd.exe";
-//        p.StartInfo.WorkingDirectory = @"../../../../Target/";
-//        p.StartInfo.Arguments = "/C dotnet run";
-//        p.Start();
-//    }
-//    p.Dispose();
-//}
+if (wrapper.Compile())
+{
+    Process p = new();
+    lock (Console.Out)
+    {
+        p = new Process();
+        p.StartInfo.FileName = "cmd.exe";
+        p.StartInfo.WorkingDirectory = @"../../../../Target/";
+        p.StartInfo.Arguments = "/C dotnet run";
+        p.Start();
+    }
+    p.Dispose();
+}
